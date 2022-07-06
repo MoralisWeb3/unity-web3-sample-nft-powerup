b8d532f74f15c1ac5f4140cf1885b3defe5a8194
dPradellMoralis <99496843+dpradell-moralis@users.noreply.github.com>
2022-07-06 12:13:39 +0200

    Hell yeah

---
+++ Assets/SkinnedMeshToMesh.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class SkinnedMeshToMesh : MonoBehaviour
+{
+    public SkinnedMeshRenderer skinnedMesh;
+    public VisualEffect vfx;
+
+    public float refreshRate;
+
+    private void Start()
+    {
+        StartCoroutine(UpdateVFX());
+    }
+
+    IEnumerator UpdateVFX()
+    {
+        while (gameObject.activeSelf)
+        {
+            Mesh mesh = new Mesh();
+            skinnedMesh.BakeMesh(mesh);
+            vfx.SetMesh("Mesh", mesh);
+
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
