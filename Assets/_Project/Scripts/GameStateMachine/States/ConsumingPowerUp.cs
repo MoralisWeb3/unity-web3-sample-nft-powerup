6c0599a00d81b9389a70da8a71797f9b1c684989
dPradellMoralis <99496843+dpradell-moralis@users.noreply.github.com>
2022-07-08 22:12:47 +0200

    Consume implemented

--- Assets/_Project/Scripts/GameStateMachine/States/ConsumingPowerUp.cs
+++ Assets/_Project/Scripts/GameStateMachine/States/ConsumingPowerUp.cs
@@ -1,19 +1,60 @@
+using System;
+using Cysharp.Threading.Tasks;
+using MoralisUnity;
+using Nethereum.Hex.HexTypes;
 using Pixelplacement;
+using TMPro;
+using UnityEngine;
 
 namespace NFT_PowerUp
 {
     public class ConsumingPowerUp : State
     {
-        // Start is called before the first frame update
-        void Start()
+        [Header("UI Elements")]
+        [SerializeField] private TextMeshProUGUI statusText;
+
+        private GameManager _gameManager;
+
+        private void Awake()
         {
-        
+            _gameManager = GetComponentInParent<GameManager>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnEnable()
         {
-        
+            ConsumePowerUp(_gameManager.currentPowerUp.myTokenId);
+        }
+
+        private async void ConsumePowerUp(string tokenId)
+        {
+            statusText.text = "Please confirm transaction in your wallet";
+    
+            var result = await ExecuteConsuming(tokenId);
+
+            if (result is null)
+            {
+                statusText.text = "Transaction failed";
+                return;
+            }
+
+            // We tell the GameManager what we minted the item successfully
+            statusText.text = "Transaction completed!";
+        }
+
+        private async UniTask<string> ExecuteConsuming(string tokenId)
+        {
+            object[] parameters = {
+                tokenId // This is the format the contract expects //TODOOOOOO
+            };
+
+            // Set gas estimate
+            HexBigInteger value = new HexBigInteger(0);
+            HexBigInteger gas = new HexBigInteger(0);
+            HexBigInteger gasPrice = new HexBigInteger(0);
+
+            string resp = await Moralis.ExecuteContractFunction(_gameManager.contractAddress, _gameManager.contractAbi, "consume", parameters, value, gas, gasPrice);
+
+            return resp;
         }
     }   
 }
